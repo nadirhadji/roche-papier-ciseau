f7040f6fe8de997a3f716bb707f70899c554d8bf
Nadir Hadji <nadir_hadji@yahoo.com>
2021-02-02 00:36:57 -0500

    premier commit

---
+++ src/rpc.java
@@ -0,0 +1,49 @@
+/**********************************************************
+ *          U Q A M   -   I N F 2 1 7 1
+ * Organisation des ordinateurs et assembleur
+ *
+ * @author Cissé Saliou
+ * @author Hadji Nadir
+ *
+ * Travail pratique 1: Jeu de roche-papier-ciseau
+ **********************************************************/
+public class rpc {
+
+    /** Les constantes d'affichage */
+
+    private static final String INTO = "-----------------------------------------------\n" +
+                                       "--- Bienvenue au jeu de roche-papier-ciseau ---\n" +
+                                       "-----------------------------------------------\n";
+
+    private static final String MSG_NB_MANCHES = "Combien de manches voulez-vous jouer?\n";
+
+    //Example de phrase a afficher : Il reste 5 manche(s) à jouer.
+
+    private static final String MSG_MANCHE_1 = "Il reste ";
+
+    private static final String MSG_MANCHE_2  = "manche(s) à jouer.\n";
+
+    //Example de phrase a afficher : JOUEUR 1, quel est votre choix? [r/p/c]
+
+    private static final String MSG_JOUEUR_1 = "JOUEUR 1";
+
+    private static final String MSG_JOUEUR_2 = "JOUEUR 2";
+
+    private static final String MSG_CHOIX = ", quel est votre choix? [r/p/c]\n";
+
+    private static final String MSG_MANCHE_NULL = "Manche nulle...\n\n";
+
+    // Example de phrase a afficher : JOUEUR 2 a gagné cette manche! Score: 1-3
+
+    private static final String MSG_VICTOIRE_MANCHE = " a gagné cette manche! Score: ";
+
+    //Example de phrase a afficher : JOUEUR 2 A GAGNÉ LE MATCH! FÉLICITATIONS!
+
+    private static final String MSG_VICTOIRE_PARTIE = " A GAGNÉ LE MATCH! FÉLICITATIONS!\n";
+
+    private static final String MSG_SCORE_FINAL = "SCORE FINAL: ";
+
+    public static void main(String[] args) {
+        //TODO
+    }
+}
